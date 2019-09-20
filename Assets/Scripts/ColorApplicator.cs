23811a0955a085ce150bcd5240110b967b6a374e
Mikey <BrainSlugs83@gmail.com>
2019-09-20 01:35:44 -0700

    Initial Commit.

---
+++ Assets/Scripts/ColorApplicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorApplicator : MonoBehaviour
+{
+    public Color Tint;
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>() ?? GetComponentInChildren<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        r.material.SetColor("_Tint", Tint);
+    }
+}
